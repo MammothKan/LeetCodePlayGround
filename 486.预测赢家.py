a85d5da3582c0734ed387a449df9eb880a864361
MammothKan <mammothkan@163.com>
2023-09-13 11:33:18 +0800

    贪心策略 ？？待解决

---
+++ 486.预测赢家.py
@@ -0,0 +1,14 @@
+#
+# @lc app=leetcode.cn id=486 lang=python3
+#
+# [486] 预测赢家
+#
+
+# @lc code=start
+class Solution:
+    def predictTheWinner(self, nums: List[int]) -> bool:
+
+        
+
+# @lc code=end
+
