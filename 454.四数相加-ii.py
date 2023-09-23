cacfa4eada381d9d12a51928d01569474297811f
MammothKan <mammothkan@163.com>
2023-09-24 00:26:46 +0800

    将4个数组两两分为两组，而后采用哈希法查找a+b+c+d=0，时间复杂度O(2n^2)

--- 454.四数相加-ii.py
+++ 454.四数相加-ii.py
@@ -7,5 +7,19 @@
 # @lc code=start
 class Solution:
     def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
+
+        # 暴力搜索法 时间复杂度O(n^4)
+        # 将4个数组分为两组，借助hashtable保存 (nums1+nums2) 的结果，而后查找 -(nums3+nums4) 的匹配结果，此法的时间复杂度为 O(2 * n^2)
+        hashtable = {}
+        for n1 in nums1:
+            for n2 in nums2:
+                hashtable[(n1+n2)] = hashtable.get((n1+n2), 0) + 1
+        
+        resules = 0
+        for n3 in nums3:
+            for n4 in nums4:
+                resules += hashtable.get(-(n3+n4), 0)
+        
+        return resules
 # @lc code=end
 
