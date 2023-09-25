ede4facc10b7966c4bf3cafe81caba477a04846f
MammothKan <mammothkan@163.com>
2023-09-25 17:01:22 +0800

    KMP算法

--- 28.找出字符串中第一个匹配项的下标.py
+++ 28.找出字符串中第一个匹配项的下标.py
@@ -24,40 +24,36 @@ class Solution:
     
         # KMP算法
         # next数组，实则为前缀表，即needle子串中相同前缀和后缀的长度
-        next = [0] * len(needle)
-        prefix_tail = 0
-        for suffix_tail in range(1, len(next)):
-            while prefix_tail > 0 and needle[prefix_tail+1] != needle[suffix_tail]:
-                prefix_tail -= 1
+        next = [-1] * len(needle)
+        prefix_tail = -1
+        for suffix_tail in range(1, len(needle)):
+            # print(f'suffix_tail={suffix_tail}')
+            while prefix_tail > -1 and needle[prefix_tail+1] != needle[suffix_tail]:
+                # prefix_tail -= 1
+                prefix_tail = next[prefix_tail]
             if needle[prefix_tail+1] == needle[suffix_tail]:
                 prefix_tail += 1
             next[suffix_tail] = prefix_tail
+        # print(next)
 
-            if needle[i] == needle[next[i-1]]:
-                next[i] = next[i-1]+1
-            elif needle[i] == needle[next[i-1]-1]:
-                next[i] = next[i-1]
-        print(next)
-
-        i, j = 0, 0
-        while i < len(haystack):
+        i, j = -1, -1
+        while i < len(haystack)-1:
             # while j < len(needle):
-            if haystack[i] == needle[j]:
+            if haystack[i+1] == needle[j+1]:
                 # i, j += 1, 1
-                # i += 1
+                i += 1
                 j += 1
             else:
-                if j != 0:
-                    i -= 1
-                    j = next[j-1]
-            i += 1
-            if j == len(needle):
-                return i - len(needle)
+                if j == -1: i += 1
+                else: j = next[j]
+            if j == len(needle)-1:
+                return i - j
         return -1
 
 
   #  "leetcode"\n"leeto"      
   # "aabaaabaaac"\n"aabaaac"
+  # "adcadcaddcadde"\n"adcadde"
 
 
         
