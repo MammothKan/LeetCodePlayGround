947b831d8d773915008df00b289cca08a48ab519
MammothKan <mammothkan@163.com>
2023-09-25 17:34:56 +0800

    待完成 双指针

---
+++ 459.重复的子字符串.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode.cn id=459 lang=python3
+#
+# [459] 重复的子字符串
+#
+
+# @lc code=start
+class Solution:
+    def repeatedSubstringPattern(self, s: str) -> bool:
+
+        sub_tail = 0
+        sub_cursor = 0
+        detect_cursor = 0
+
+        for detect_cursor in range(len(s)):
+            
+
+
+
+# @lc code=end
+
